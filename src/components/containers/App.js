7e83ef6ba75048c10d0ec4d492099db70c9a6b44
Moses Samuel <mosamuel98@gmail.com>
2017-05-20 01:22:51 -0700

    refactor App component

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -4,19 +4,20 @@ import SearchBar from '../SearchBar'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import '../../styles/App.css';
 
+/* TODO: Render MuiThemeProvider on the server for much needed utility 
+(currently, you have to prepend a MuiThemProvider component to each material-ui component #soTedious)
+*/
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <MuiThemeProvider >
-                    <div className="App-header">
-                        <img src={logo} className="App-logo" alt="logo" />
-                        <h2>SFS Booklet</h2>
-                    </div>
-                    <div className="App-intro">
-                        <SearchBar />
-                    </div>
-                </MuiThemeProvider>
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>SFS Booklet</h2>
+                </div>
+                <div className="App-intro">
+                    <SearchBar />
+                </div>
             </div>
    );
  }
